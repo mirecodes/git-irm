f047d5516095334d96c4e7c408bac3e46fbc72c7
Hyesung Lee <mirecodes@gmail.com>
2024-03-06 17:20:27 +0100

    implemented all the codes

--- lab01/hs/mC/mC.ino
+++ lab01/hs/mC/mC.ino
@@ -3,6 +3,7 @@ int dl, mlt, incoming;
 
 // define an array to access the LED Pins
 int led_pins[10] = {12, 27, 33, 15, 32, 14, 22, 23};
+int switch_pin = 21;
 
 void setup()
 {
@@ -15,6 +16,8 @@ void setup()
   for (int i=0; i<8; i++) {
     pinMode(led_pins[i], OUTPUT);
   }
+
+  pinMode(switch_pin, INPUT);
 
   //_________________Begin - Part A_______________
 
@@ -40,25 +43,36 @@ void loop()
     mlt = incoming % (1<<4);
   }
 
+  int turnon = digitalRead(switch_pin);
+
   //_________________End - Part B_________________
 
-  //_________________Begin - Part C_______________
-  // loop over all output (LED) pins and set state to HIGH/LOW
-  // e.g. digitalWrite(1,LOW) means you set the digital pin 1 to LOW.
-  for (j = 0; j < 8; j++)
-  {
-    for (i = 0; i < 8; i++)
-      digitalWrite(led_pins[i], LOW);
-    digitalWrite(led_pins[j], HIGH);
-    delay(dl * mlt);
+  if (turnon == HIGH) {
+    //_________________Begin - Part C_______________
+    // loop over all output (LED) pins and set state to HIGH/LOW
+    // e.g. digitalWrite(1,LOW) means you set the digital pin 1 to LOW.
+    
+    for (j = 0; j < 8; j++)
+    {
+      for (i = 0; i < 8; i++)
+        digitalWrite(led_pins[i], LOW);
+      digitalWrite(led_pins[j], HIGH);
+      delay(dl * mlt);
+    }
+    delay(dl * mlt * 3);
+    //_________________End - Part C_________________
   }
-  delay(dl * mlt * 3);
-  //_________________End - Part C_________________
 
-  //_________________Begin - Part D_______________
-
-  /*
+  else {
+    //_________________Begin - Part D_______________
+    for (i = 0; i < 8; i++) {
+      int res = incoming & (1 << i);
+      if (res)
+        digitalWrite(led_pins[i], HIGH);
+      else
+        digitalWrite(led_pins[i], LOW);
+    }
+    //_________________End - Part D_________________
+  }
   delay(10);
-  */
-  //_________________End - Part D_________________
 }
--- lab01/hs/mP/mP.c
+++ lab01/hs/mP/mP.c
@@ -3,20 +3,46 @@
 #include "feather_serial.h"
 
 // ***** print_bits() can be implemented here ***** //
+void print_bits(uint8_t arg_word)
+{
+	printf("hex: 0x%02x, bin: ", arg_word);
 
+	// only a simple implementation of a for loop is given
+	// you are free to use it or solve the problem in another way
+
+	for (int i=1; i>-1; i--) {
+		char bin_digits[4] = {};
+
+		bin_digits[0] = 0 != (arg_word & 1<<(3+i*4));
+		bin_digits[1] = 0 != (arg_word & 1<<(2+i*4));
+		bin_digits[2] = 0 != (arg_word & 1<<(1+i*4));
+		bin_digits[3] = 0 != (arg_word & 1<<(0+i*4));
+
+		for (int j=0; j<4; j++) {
+			printf("%d", bin_digits[j]);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
 // *********************************************** //
 
 int32_t main()
 {
   int n;
   // initialization of serial communication, port and baud rate are specified
-  int fd = serialport_init("/dev/ttyUSB0", 115200);
+  int fd = serialport_init("/dev/cu.usbserial-022F296B", 115200);
   uint8_t arg = (uint8_t)245;
 
   while (1)
   {
     // ***** your code goes here ***** //
+    int a, b = 0; 
+    scanf("%d %d", &a, &b);
 
+    uint8_t arg = (a+b) & ((1<<8) - 1);
+
+    print_bits(arg);
     // ******************************* //
 
     // send arg via serial communication to the mC
