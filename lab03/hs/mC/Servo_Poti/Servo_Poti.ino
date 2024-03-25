9b0611ad09cbde7ebf0f47130b2caac5e45f861a
Hyesung Lee <mirecodes@gmail.com>
2024-03-25 13:00:00 +0100

    LAB3: uploaded basic ocde of

--- lab03/hs/mC/Servo_Poti/Servo_Poti.ino
+++ lab03/hs/mC/Servo_Poti/Servo_Poti.ino
@@ -3,6 +3,9 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
+// declare pins
+int POTI = A0;
+
 // declare variables
 int servo_num = 0;
 int servo_freq = 50;
@@ -15,9 +18,11 @@ void setup() {
   // _______________ Begin - Setup _______________
 
   // Begin the serial communication
+  Serial.begin(115200);
   // Begin PWM communication and set servo frequency
-
-  
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
   // _______________ End - Setup _______________
   
 }
@@ -26,6 +31,17 @@ void loop() {
   // _______________ Begin - Loop _______________
 
   // Read values from the analog pin and map/scale them to the movment range of the servo.
+  int potential = analogRead(POTI);
+
+  int pwm_microsec = map(potential, 0, 4095, 550, 2400);
+
+  Serial.print("poential: ");
+  Serial.print(potential);
+  Serial.print(", microsec: ");
+  Serial.println(pwm_microsec);
+
+  pwm.writeMicroseconds(servo_num, pwm_microsec);
+
   // Optionally display the reading from the potentiometer on the serial monitor
   // Set the servo position according to the mapped/scaled value
   
