a46d6940ca725bccaef5431d8712244259611d34
Hyesung Lee <mirecodes@gmail.com>
2024-04-18 17:15:49 +0200

    complete task 3.1

--- lab04/hs/mP/hall_sensor.c
+++ lab04/hs/mP/hall_sensor.c
@@ -5,4 +5,12 @@
 float hall_sensor_get_field(float voltage, float voltage_0)
 {
    // Insert your code here
+   int temperature = 25;
+   int sensitivity_25 = 30;
+   float stc = 0.0012;
+   float sensitivity = sensitivity_25*(1+stc*(temperature-25));
+   float B = (voltage - voltage_0) / sensitivity * 1000; // unit: [mT]
+   printf("measured magnetic field= %.3f [mT]\n", B);
+
+   return B;
 }
